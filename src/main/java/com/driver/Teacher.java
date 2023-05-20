33776afa9970d2fe7f58347e512e2136b93a40cb
gundasriharsha <harshagunda2002@gmail.com>
2023-05-20 18:47:52 +0530

    First commit

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -1,7 +1,11 @@
 package com.driver;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class Teacher
     private String name;
 
     private int numberOfStudents;
@@ -11,4 +15,11 @@ public class Teacher {
     public Teacher() {
 
     }
+
+    public Teacher(String name, int numberOfStudents, int age) {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+        this.age = age;
+    }
 }
+
